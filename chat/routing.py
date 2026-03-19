37c1e20557bd2d90a25e2bec638c4934d5480937
BugsR3AP3R <arbensvital2@gmail.com>
2026-03-19 12:14:00 -0400

    Delete chat directory

--- chat/routing.py
+++
@@ -1,6 +0,0 @@
-from django.urls import re_path
-from . import consumers
-
-websocket_urlpatterns = [
-    re_path(r'ws/classroom/(?P<room_id>\w+)/$', consumers.ClassroomConsumer.as_asgi()),
-]
--- chat/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('classroom/<slug:course_slug>/', views.classroom, name='classroom'),
-]
