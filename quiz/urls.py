4878d9d7a0585830a9ff7b1028320b406926f3ec
BugsR3AP3R <arbensvital2@gmail.com>
2026-03-19 12:14:57 -0400

    Delete quiz directory

--- quiz/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('<int:quiz_id>/',              views.quiz_detail, name='quiz_detail'),
-    path('<int:quiz_id>/take/',         views.take_quiz,   name='take_quiz'),
-    path('result/<int:attempt_id>/',    views.quiz_result, name='quiz_result'),
-]
